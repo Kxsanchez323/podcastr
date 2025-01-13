9d5a04959b397dfdf4fdac62c1f2fd6cb8f32b00
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2025-01-12 19:10:55 -0500

    Completed Convex setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ConvexClientProvider } from "./providers/ConvexProvider";
 
 export const metadata: Metadata = {
   title: "Podcaster",
@@ -18,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
+        <ConvexClientProvider>
         {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
